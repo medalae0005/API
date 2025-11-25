6bff6398287f309889609a265517953ea58befcf
medalae0005 <elbouazzaoui.malae.solicode@gmail.com>
2025-11-25 11:27:42 +0000

    Project...

---
+++ Projet/script.js
@@ -0,0 +1,74 @@
+const container = document.getElementById("cardContainer");
+const continentSelect = document.getElementById("my-select");
+let countriesData = [];
+const searchInput = document.getElementById("searchInput");
+const continentCountDiv = document.getElementById("continentCount");
+
+// Load data
+fetch("https://countries-api-hsak.onrender.com/api/countries")
+  .then(res => res.json())
+  .then(data => {
+    countriesData = data;
+    updateContinentCount(data);
+    displayCountries(data);
+  })
+  .catch(err => console.error("Erreur lors du chargement des pays :", err));
+
+// Display countries
+function displayCountries(countries) {
+  container.innerHTML = "";
+
+  countries.forEach(country => {
+    const card = document.createElement("div");
+    card.className = "card";
+
+    card.innerHTML = `
+      <h3>${country.name}</h3>
+      <p>Capital: ${country.capital}</p>
+      <p>Language: ${country.language}</p>
+      <p>Continent: ${country.continent}</p>
+      <img src="${country.flag}" alt="Flag of ${country.name}" class="flag-img">
+    `;
+
+    card.onclick = () => {
+      localStorage.setItem("selectedCountry", JSON.stringify(country));
+      window.location.href = "country-details.html";
+    };
+
+    container.appendChild(card);
+  });
+}
+
+// Update Continent...
+function updateContinentCount(data) {
+  const count = {};
+  data.forEach(c => {
+    count[c.continent] = (count[c.continent] || 0) + 1;
+  });
+
+  for (let continent in count) {
+    continentCountDiv.innerHTML += `<p>${continent}: <b>${count[continent]}</b></p>`;
+  }
+}
+
+// Search ...
+searchInput.addEventListener("input", () => {
+  const value = searchInput.value.toLowerCase();
+
+  const filtered = countriesData.filter(country =>
+    country.name.toLowerCase().includes(value)
+  );
+
+  displayCountries(filtered);
+});
+
+// Filter by continent
+continentSelect.addEventListener("change", () => {
+  const selected = continentSelect.value;
+
+  const filtered = selected === "continent"
+    ? countriesData
+    : countriesData.filter(c => c.continent === selected);
+
+  displayCountries(filtered);
+});
